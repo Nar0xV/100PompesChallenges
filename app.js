481a41c3607816b3a5c333fd541e8e81691731ff
Nar0xV <35712771+Nar0xV@users.noreply.github.com>
2026-03-27 20:40:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -221,7 +221,7 @@ function renderAll() {
     document.getElementById("btnComplete").textContent = "✓ SÉANCE VALIDÉE !";
     document.getElementById("btnComplete").disabled = true;
     document.querySelector(".complete-note").textContent = `Temps : ${formatTime(sessions[today].time || 0)} — Bravo ! 🏆`;
-   document.getElementById("btnCancel").disabled = true;
+   document.getElementById("btnCancel").disabled = false;
    document.getElementById("btnCancel").classList.remove("hidden");
 } else {
   // Bouton reset désactivé
